416bdfe3258373e23e1e4e1aa953e63708fa9382
Musab <67883529+Musab45@users.noreply.github.com>
2025-08-15 00:51:32 +0500

    Update apiKey.dart

--- supabase_authentication/lib/constants/apiKey.dart
+++ supabase_authentication/lib/constants/apiKey.dart
@@ -2,8 +2,8 @@
 
 class Apikey {
   final String _supabase_api =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6a2R1eGhpaHJ5b2RubXl0ZGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MjczNDQsImV4cCI6MjA2NDEwMzM0NH0.ynCCIj2MTUGZbrqrGvio6cH9iz1rbDAVTJETGeRNnmU';
-  final String _supabase_url = 'https://kzkduxhihryodnmytdcq.supabase.co';
+      'api_here';
+  final String _supabase_url = 'url_here';
 
   String get supabase_api => _supabase_api;
   String get supabase_url => _supabase_url;
