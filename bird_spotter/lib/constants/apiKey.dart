6266e7f8a571f15f6eb148bbf2a43d7ea48d8aa9
Musab <67883529+Musab45@users.noreply.github.com>
2026-03-07 01:36:49 +0500

    Update apiKey.dart

--- bird_spotter/lib/constants/apiKey.dart
+++ bird_spotter/lib/constants/apiKey.dart
@@ -2,8 +2,8 @@
 
 class Apikey {
   final String _supabase_api =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kd3Z5YXN5aGl3Y2Vtb2hqZHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4Njg5NTgsImV4cCI6MjA3MTQ0NDk1OH0.a_MQSC7IQXW02a-6lBnorEA6Cu74OUtJa9kkuiycI1U';
-  final String _supabase_url = 'https://odwvyasyhiwcemohjdzu.supabase.co';
+      'apikey';
+  final String _supabase_url = 'url';
 
   String get supabase_api => _supabase_api;
   String get supabase_url => _supabase_url;
